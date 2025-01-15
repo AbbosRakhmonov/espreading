828b4f486ad8ce82943972b08266ddc01d69e2de
acceleratorusa <acceleratorusa@gmail.com>
2025-01-15 17:16:11 -0600

    hh

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,11 @@ const { connect } = require("mongoose");
 
 module.exports = async () => {
   try {
-    return await connect(process.env.MONGO_URI);
+    let mongoURI =
+      process.env.NODE_ENV === "production"
+        ? `mongodb+srv://abbosraxmonov2001:${process.env.MONGO_PASS}@espreading.fzesn.mongodb.net/?retryWrites=true&w=majority&appName=espreading`
+        : process.env.MONGO_URI;
+    return await connect(mongoURI);
   } catch (error) {
     console.error(`Error: ${error.message}`.red);
     process.exit(1);
