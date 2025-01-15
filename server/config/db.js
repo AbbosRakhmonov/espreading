acf2148912207fbe411eaf0b6ced72abbb66c6cf
acceleratorusa <acceleratorusa@gmail.com>
2025-01-15 17:22:59 -0600

    a

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,8 @@ const { connect } = require("mongoose");
 
 module.exports = async () => {
   try {
+    console.log(process.env.NODE_ENV);
+
     let mongoURI =
       process.env.NODE_ENV === "production"
         ? `mongodb+srv://abbosraxmonov2001:${process.env.MONGO_PASS}@espreading.fzesn.mongodb.net/?retryWrites=true&w=majority&appName=espreading`
