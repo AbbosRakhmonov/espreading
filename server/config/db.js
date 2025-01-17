1de07c6a87d111661cb3e36d6dffa5c32029080d
acceleratorusa <acceleratorusa@gmail.com>
2025-01-17 13:48:05 -0600

    jonga tegdi

--- server/config/db.js
+++ server/config/db.js
@@ -8,6 +8,8 @@ module.exports = async () => {
       process.env.NODE_ENV === "production"
         ? `mongodb+srv://abbosraxmonov2001:${process.env.MONGO_PASS}@espreading.fzesn.mongodb.net/?retryWrites=true&w=majority&appName=espreading`
         : process.env.MONGO_URI;
+    console.log(mongoURI);
+
     return await connect(mongoURI);
   } catch (error) {
     console.error(`Error: ${error.message}`.red);
